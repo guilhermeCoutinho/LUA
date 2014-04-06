31be95e8bf4551e4283b2b9f1ec1c7d196e56a39
guilhermeCoutinho <guilhermejcoutinho@gmail.com>
2014-04-06 01:34:27 -0300

    TRABALHO LP

--- distanciaEntrePontos.lua
+++ distanciaEntrePontos.lua
@@ -3,9 +3,9 @@ function dist ( p1 ,p2 )
   return math.sqrt ( ( p2.x - p1.x) * ( p2.x - p1.x) + (p2.y - p1.y)*(p2.y - p1.y) )
 end
 
-p3 = {x = 3,
+p1 = {x = 3,
      y = 4}
-p4 = {x = 0,
+p2 = {x = 0,
      y = 0}
 
 print (dist(p1,p2))
