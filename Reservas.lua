ea53129a509ecc432a428951e8d20180d427d2ed
guilhermeCoutinho <guilhermejcoutinho@gmail.com>
2014-04-07 12:52:19 -0300

    TRABALHO LP

--- Reservas.lua
+++ Reservas.lua
@@ -4,6 +4,67 @@ local reserva = {
 		termino = {}
 	}
 
+
+function imprime_reservas( )
+
+	for j = 1 , 32 do 
+		local isFirstTime = true
+			local aux = {}
+
+		for i,v in ipairs(reserva.dia) do
+			if ( j == v ) then 
+
+				if (isFirstTime) then
+					io.write (">  " , v , "\n")
+					isFirstTime = false
+				end
+				for k,l in ipairs(reserva.inicio) do
+
+					if ( i == k) then 
+						aux[#aux+1] = reserva.inicio[k]
+						aux[#aux+1] = reserva.termino[k]
+					end
+
+				end
+
+
+			end
+
+		end
+
+		local aux_numerico = {}
+
+		for m,n in ipairs(aux) do
+			aux_numerico[m] = string.sub(n , 1 , 2) * 60 + string.sub(n,4,5)
+		end
+
+
+		for i = 1 , #aux/2 do
+
+			local menor = 24*60
+			local menor_inicio
+			local menor_termino
+
+			for i,v in ipairs(aux_numerico) do
+				if (menor > v ) then
+					menor = v
+					menor_inicio = i
+					menor_termino = i+1
+				end
+			end
+
+			io.write("    |" , aux[menor_inicio] , " - " , aux[menor_termino] , "\n")
+			aux_numerico[menor_inicio] = 24*60
+			aux_numerico[menor_termino] = 24*60
+		end
+
+	end
+
+
+end
+
+
+
 function verificaDisponibilidade( user_dia, user_inicio_string , user_termino_string  )
 
 	local isDisponivel = true
@@ -47,13 +108,10 @@ function start ( )
 	io.write ("Dia: ")
 	user_dia = io.read()
 
-	if ( user_dia == '0' ) then
-		for i,v in ipairs(reserva.dia) do
-			io.write(">  ", v , 
-				"\n  | " , reserva.inicio[i],
-				" - " , reserva.termino[i],
-				"\n" )
-		end
+	user_dia = user_dia + 1 - 1 -- transforma user_dia em um inteiro.
+
+	if ( user_dia == 0 ) then
+		imprime_reservas()
 	else
 		io.write("Inicio: ")
 		user_inicio_string = io.read()
